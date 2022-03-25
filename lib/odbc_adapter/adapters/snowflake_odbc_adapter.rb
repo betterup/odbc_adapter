04e1263dcb76c6c2a5fb84d3b052844d6e37691a
Jon Marshall <jon.marshall@betterup.co>
2022-03-25 17:27:18 -0400

    Turn off prepared statements

--- lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
@@ -6,6 +6,11 @@ module ODBCAdapter
     class SnowflakeODBCAdapter < ActiveRecord::ConnectionAdapters::ODBCAdapter
       include ODBCAdapter::Snowflake::SchemaStatements
 
+      # Copy the null adapter because prepared statements aren't behaving for now
+      def prepared_statements
+        false
+      end
+
       # Snowflake is fairly similar to PG
       def arel_visitor
         Arel::Visitors::PostgreSQL.new(self)
