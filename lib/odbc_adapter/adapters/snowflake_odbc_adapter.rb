d9fe93cdf67f90bb5eb364bace4cf2b785666b01
Jon Marshall <jon.marshall@betterup.co>
2022-03-25 17:24:55 -0400

    Tweaking again to be more similar to PG

--- lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
@@ -6,10 +6,6 @@ module ODBCAdapter
     class SnowflakeODBCAdapter < ActiveRecord::ConnectionAdapters::ODBCAdapter
       include ODBCAdapter::Snowflake::SchemaStatements
 
-      class BindSubstitution < Arel::Visitors::ToSql
-        include Arel::Visitors::BindVisitor
-      end
-
       # Snowflake is fairly similar to PG
       def arel_visitor
         Arel::Visitors::PostgreSQL.new(self)
