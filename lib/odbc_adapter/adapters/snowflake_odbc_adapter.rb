c97c2381ebb895bc7d895334368309c2d7ccc150
Jon Marshall <jon.marshall@betterup.co>
2022-03-29 12:36:41 -0400

    Take the initializer code from another PR and move it into the actual adapter

--- lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/snowflake_odbc_adapter.rb
@@ -11,9 +11,21 @@ module ODBCAdapter
         false
       end
 
+      # Quoting needs to be changed for snowflake
+      def quote_column_name(name)
+        name.to_s
+      end
+
       # Snowflake is fairly similar to PG
       def arel_visitor
         Arel::Visitors::PostgreSQL.new(self)
+      end
+
+      private
+
+      # Override dbms_type_cast to get the values encoded in UTF-8
+      def dbms_type_cast(_columns, values)
+        values.each { |row| row.each_index { |idx| row[idx] = row[idx].force_encoding('UTF-8') if row[idx].is_a?(String) } }
       end
     end
   end
